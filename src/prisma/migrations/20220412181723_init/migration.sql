359bd4b5d1a598a26a45f55866d1c8b33e1ee2c0
Matheus Santos <matheusgabriel.18@gmail.com>
2022-04-12 17:04:13 -0300

    add prisma setup

---
+++ src/prisma/migrations/20220412181723_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `occupation` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
